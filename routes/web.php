ecd4825b540ef01f1e61c33b44b3fe176ff2031f
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-04-26 19:08:42 +0200

    Add jeroennoten/laravel-adminlte Package for dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('index');
