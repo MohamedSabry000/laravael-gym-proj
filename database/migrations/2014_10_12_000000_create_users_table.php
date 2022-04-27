7046d326c149769bf10e2b668abf220969ca477e
Mohammed Atef <mohammed.eldegwy2016@gmail.com>
2022-04-27 06:54:34 +0200

    add user seeder and edit user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,31 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+
+            $table->string('email')->unique()->index();
+            $table->boolean('is_verifications')->nullable()->default(0);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+
+            $table->string('national_id')->unique()->nullable();
+
+            $table->string('password')->nullable();
             $table->rememberToken();
+
+            $table->enum('gender', ['male', 'female'])->default('male');
+
+            $table->string('profile_image')->nullable();
+            $table->date('birth_date')->nullable();
+
+            $table->string('total_sessions')->nullable();
+            $table->string('remain_session')->nullable();
+
+            $table->timestamp('last_login_at')->useCurrent();
+
+            $table->softDeletes();
             $table->timestamps();
+
+            // $table->foreignId('gym_id')->nullable()->constrained();
         });
     }
 
