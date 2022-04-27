d67861bcab54aa77433afa44870d7e977666cc57
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-04-27 21:26:15 +0200

    permission seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,23 +7,55 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Permission\Traits\HasRoles;
+use Cog\Contracts\Ban\Bannable as BannableContract;
+use Cog\Laravel\Ban\Traits\Bannable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
-    use HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasRoles;
+    // use Bannable;
+
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+    #=======================================================================================#
+    #			                    To allow insert in table                              	#
+    #=======================================================================================#
     protected $fillable = [
         'name',
         'email',
+        'is_verifications',
+        'national_id',
         'password',
+        'gender',
+        'profile_image',
+        'birth_date',
+        'total_sessions',
+        'remain_session',
+        'last_login_at',
+        'city_id',
+        'gym_id',
+        'updated_at',
     ];
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+    public function gym()
+    {
+        return $this->belongsTo(Gym::class);
+    }
+    public function TrainingSessions()
+    {
+        return $this->belongsToMany(TrainingSession::class);
+    }
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -43,4 +75,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getProfileImageFileAttribute()
+    {
+        if ($this->attributes['profile_image']) {
+            return $this->attributes['profile_image'];
+        } else {
+            return 'avatar.png';
+        }
+    }
 }
