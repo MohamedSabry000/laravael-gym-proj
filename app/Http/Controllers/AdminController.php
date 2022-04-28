e9372839c5d6ee45e06b6747ec1f1cd78b2b3ee0
SamarNegm <negmsama890@gmail.com>
2022-04-28 02:41:59 +0200

    city view addes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
   
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\City;
 use DataTables;
 
 class AdminController extends Controller
@@ -51,6 +52,24 @@ class AdminController extends Controller
         }
         
         return view('allUsers.show');
+    }
+    public function showCites(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = City::select('*');
+            return DataTables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+     
+                           $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+    
+                            return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+        
+        return view('city.list');
     }
 
 }
--- resources/views/city/list.blade.php
+++ resources/views/city/list.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <div class="content-wrapper content-inner-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <div class="container-fluid">
@@ -37,42 +37,17 @@
                     </div>
                 </div>
                 <div class="card-body p-0">
-                    <table class="table table-striped projects" id="proj">
+                    <table class="table table-striped projects data-table ">
                         <thead>
                             <tr>
                                 <th class="project-state"> ID </th>
                                 <th class="project-state"> City Name</th>
-                                <th class="project-state"> City Manager Name</th>
-                                <th class="project-state">Created at</th>
+                                <th class="project-state"> City Manager Id</th>
                                 <th class="project-state"></th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($allCities as $city)
-                                <tr id="cid{{ $city->id }}">
 
-                                    <td class="project-state">{{ $city->id }}</td>
-                                    <td class="project-state">{{ $city->name }}</td>
-                                    @if ($city->manager == null)
-                                        <td class="project-state">This city has no Manager</td>
-                                    @else
-                                        <td class="project-state">{{ $city->manager->name }}</td>
-                                    @endif
-                                    <td class="project-state">{{ $city->created_at->format('d - M - Y') }}</td>
-                                    <td class="project-actions project-state">
-                                        <a class="btn btn-info btn-sm" href="{{ route('city.show', $city->id) }}">
-                                            <i class="fa fa-eye"></i>
-                                        </a>
-                                        <a class="btn btn-warning btn-sm text-white"
-                                            href="{{ route('city.edit', $city->id) }}">
-                                            <i class="fas fa-pencil-alt"></i></a>
-
-                                        <a href="javascript:void(0)"
-                                            onclick="deleteCity({{ $city->id }},{{ $city->manager_id }})"
-                                            class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
-                                    </td>
-                                </tr>
-                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -80,6 +55,49 @@
             </div>
             <!-- /.card -->
         </section>
+        @section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+    <!-- <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet"> -->
+    <!-- <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
+    <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+
+    <!-- <style>
+        .content-wrapper{
+            width: 90% !important;
+            margin: auto !important;
+        }
+    </style> -->
+    @stop
+    @section('js')
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+        <!-- <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script> -->
+        <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> -->
+        <!-- <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script> -->
+        <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+        
+
+        <script type="text/javascript">
+            $(document).ready( function () {
+                $('#myTable').DataTable();
+            } );
+            $(function () {
+                
+                var table = $('.data-table').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    ajax: "{{ route('showCites') }}",
+                    columns: [
+                        {data: 'id', name: 'id'},
+                        {data: 'name', name: 'manager_id'},
+                        {data: 'action', name: 'action', orderable: false, searchable: false},
+                    ]
+                });
+                
+            });
+        </script>
+    @stop
+
     </div>
     <!-- /.content-wrapper -->
     <script>
