195e92fefdd8cba787c66306abf4495ea587c5d5
kakorotto <paulaatiya@gmail.com>
2022-04-30 01:36:28 +0200

    update on add city manager

--- resources/views/cityManager/edit.blade.php
+++ resources/views/cityManager/edit.blade.php
@@ -1,97 +1,92 @@
 @extends('adminlte::page')
 @section('content')
 
-
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper pb-4">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper pb-4">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
 
                     <!-- Errors Section -->
-                @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
-                <div class="col-sm-6">
-                    <h1>City Manger</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Edit City Manger</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- Main content -->
-    <section class="content">
-        <form action="{{ route('cityManager.update', [$singleUser['id']]) }}" method="post" enctype="multipart/form-data"
-            class="w-75 m-auto">
-             @method('PUT')
-             @csrf
-          
-
-            <div class="row">
-                <div class="col-md-12">
-                    @if (session('status'))
-                    <h6 class="alart-success">{{ session('status') }}</h6>
-                    @endif
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h3 class="card-title">Editing</h3>
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                            </div>
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="name">Name</label>
-                                <input autofocus required minlength="3" maxlength="50" type="text" id="name"
-                                    value="{{$singleCoach->name}}" class="form-control" value="" name="name">
+                    @endif
+
+                    <div class="col-sm-6">
+                        <h1>Gym City Manger</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Edit City Manger</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- Main content -->
+        <section class="content">
+            <form action="{{ route('cityManager.update', [$singleUser['id']]) }}" method="post" enctype="multipart/form-data"
+                class="w-75 m-auto">
+                @method('PUT')
+                @csrf
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Editing</h3>
+                                <div class="card-tools">
+                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"
+                                        title="Collapse">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input required type="email" id="email" class="form-control" name="email"
-                                    value="{{$singleCoach->email}}">
-                            </div>
-                            <div class="form-group">
-                                <label for="city">City</label>
-                                <select required class=" form-control" name="city_id" id="city">
-                                    <optgroup label="Available City">
-                                        @foreach ($cities as $city)
-                                        <option value={{ $city->id }}
-                                            <?php echo $city->id == $singleCoach->city_id? 'selected': '';  ?>>
-                                            >{{ $city->name }}</option>
-                                        @endforeach
-                                    </optgroup>
-                                </select>
-                            </div>
-                            <input type="hidden" name="profile_image_saved" value="{{$singleCoach->profile_image}}">
-                            <div class="form-group">
-                                <label class="form-label" for="image">Image Cover</label>
-                                <input type="file" class="form-control" id="image" name="profile_image">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" id="name" class="form-control" value="{{ $singleUser->name }}"
+                                        name="name">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="pass">Password</label>
+                                    <input type="password" id="pass" class="form-control"
+                                        value="{{ $singleUser->password }}" name="password">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" id="email" class="form-control" value="{{ $singleUser->email }}"
+                                        name="email">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label" for="image">Upload Image</label>
+                                    <input type="file" class="form-control" id="image" name="profile_image"
+                                        value="{{ asset($singleUser->profile_image) }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="nationalID">National ID</label>
+                                    <input type="text" id="nationalID" class="form-control" name="national_id"
+                                        value="{{ $singleUser->national_id }}">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <a href="#" class="btn btn-secondary">Cancel</a>
-                    <input type="submit" value="Save Changes" class="btn btn-success float-right">
+                <div class="row">
+                    <div class="col-12">
+                        <a href="#" class="btn btn-secondary">Cancel</a>
+                        <input type="submit" value="Save Changes" class="btn btn-warning float-right">
+                    </div>
                 </div>
-            </div>
-        </form>
-    </section>
-</div>
+            </form>
+        </section>
+    </div>
 @endsection
--- resources/views/cityManager/show.blade.php
+++ resources/views/cityManager/show.blade.php
@@ -1,6 +1,5 @@
 @extends('adminlte::page')
 @section('content')
-
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -8,48 +7,50 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Show</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Show</li>
-                    </ol>
+                    <h4>Show City Manager Number {{$singleUser->id}}</h4>
                 </div>
             </div>
         </div><!-- /.container-fluid -->
     </section>
-
     <!-- Main content -->
     <section class="content">
-        <!-- Default box -->
-        <div class="card card-solid">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-12 col-sm-6">
-                        <h6 class="d-inline-block d-sm-none">Coach Name</h6>
-                        <img class="img-fluid" src="{{$singleCoach->profile_image}}">
-                    </div>
-                    <div class="col-12 col-sm-6 d-flex  align-items-center">
-                        <div>
-                            <p class="my-3">{{$singleCoach->id}}</p>
-                            <p class="my-3">{{$singleCoach->name}}</p>
-                            <p class="my-3">{{$singleCoach->email}}</p>
-                            <p class="my-3">{{$cityCoach}}</p>
-                            <p class="my-3">{{$singleCoach->created_at}}</p>
 
-                        </div>
-                    </div>
-                </div>
+        <!-- Default box -->
+        <div class="card">
+
+            <div class="card-body p-0">
+                <table class="table table-striped projects">
+                <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>City Manager Name</th>
+                            <th>Email</th>
+                            <th>Profile Picture</th>
+                            <th>National ID</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                    
+                        <tr>
+                            <td>{{$singleUser->id}}</td>
+                            <td>{{$singleUser->name}} </td>
+                            <td>{{$singleUser->email}} </td>
+                            <td><img alt="Avatar" class="table-avatar" src="{{$singleUser->profile_image}}"></td>
+                            <td>{{ $singleUser->national_id }} </td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+
+                       
+                    </tbody>
+                </table>
             </div>
             <!-- /.card-body -->
         </div>
         <!-- /.card -->
 
     </section>
-    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
-
-
 @endsection
