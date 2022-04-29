9e419b6ed1b96810d04b2aa8958a394d6de303c5
Mohammed Atef <mohammed.eldegwy2016@gmail.com>
2022-04-29 09:39:35 +0200

    user api signin and create token

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\Api\SessionsController;
+use App\Http\Controllers\Api\PackagesController;
+use App\Http\Controllers\Api\AuthController;
+use App\Models\User;
+use App\Models\TrainingSession;
+use App\Models\TrainingPackage;
+
+
+use App\Http\Controllers\Api\EmailVerificationController;
+use App\Http\Controllers\Auth\VerificationController;
+use Illuminate\Support\Facades\Auth;
+/*
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +31,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('signin', 'signin');
+    Route::post('signup', 'signup');
+    Route::get('logout', 'logout')->middleware('auth:sanctum');
+});
+
+
+
+
+
+
+
+
